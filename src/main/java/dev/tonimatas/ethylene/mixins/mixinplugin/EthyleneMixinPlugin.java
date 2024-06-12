c4f9ab3ffc97dfe2b281faa555b4417e3107dd92
TonimatasDEV <amatascubero@gmail.com>
2024-06-12 16:39:51 +0200

    Fix constructor

---
+++ src/main/java/dev/tonimatas/ethylene/mixins/mixinplugin/EthyleneMixinPlugin.java
@@ -0,0 +1,101 @@
+package dev.tonimatas.ethylene.mixins.mixinplugin;
+
+import dev.tonimatas.ethylene.mixins.mixinplugin.annotations.*;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.*;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import org.spongepowered.asm.util.Annotations;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
+import java.util.function.Consumer;
+
+
+public class EthyleneMixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+        
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return "";
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return false;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return List.of();
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+    
+    // Start - This code is created by C0D3-M4513R
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+        AnnotationNode changeSuperClass = Annotations.getInvisible(targetClass, ChangeSuperClass.class);
+        if (changeSuperClass != null) {
+            String oldOwner = targetClass.superName;
+            targetClass.superName = Annotations.<Type>getValue(changeSuperClass).getInternalName();
+            transformCalls(targetClass, call -> {
+                if (call.getOpcode() == Opcodes.INVOKESPECIAL && call.owner.equals(oldOwner)) {
+                    call.owner = targetClass.superName;
+                }
+            });
+        }
+        for (ListIterator<MethodNode> it = targetClass.methods.listIterator(); it.hasNext(); ) {
+            MethodNode method = it.next();
+            if (Annotations.getInvisible(method, SuperConstructorStub.class) != null) {
+                it.remove();
+                transformCalls(targetClass, call -> {
+                    if (call.name.equals(method.name) && call.desc.equals(method.desc)) {
+                        call.setOpcode(Opcodes.INVOKESPECIAL);
+                        call.name = "<init>";
+                        call.owner = targetClass.superName;
+                    }
+                });
+                continue;
+            }
+            if (Annotations.getInvisible(method, SelfConstructorStub.class) != null) {
+                it.remove();
+                transformCalls(targetClass, call -> {
+                    if (call.name.equals(method.name) && call.desc.equals(method.desc)) {
+                        call.setOpcode(Opcodes.INVOKESPECIAL);
+                        call.name = "<init>";
+                        call.owner = targetClass.name;
+                    }
+                });
+                continue;
+            }
+            if (Annotations.getInvisible(method, NewConstructor.class) != null) {
+                method.name = "<init>";
+            }
+        }
+    }
+
+    private void transformCalls(ClassNode classNode, Consumer<MethodInsnNode> consumer) {
+        for (MethodNode method : classNode.methods) {
+            for (AbstractInsnNode insn : method.instructions) {
+                if (insn instanceof MethodInsnNode call) {
+                    consumer.accept(call);
+                }
+            }
+        }
+    }
+    // End
+}
--- src/main/java/dev/tonimatas/ethylene/mixins/world/item/trading/MerchantRecipeMixin.java
+++ src/main/java/dev/tonimatas/ethylene/mixins/world/item/trading/MerchantRecipeMixin.java
@@ -1,7 +1,10 @@
 package dev.tonimatas.ethylene.mixins.world.item.trading;
 
 import dev.tonimatas.ethylene.link.world.item.trading.MerchantOfferLink;
+import dev.tonimatas.ethylene.mixins.mixinplugin.annotations.NewConstructor;
+import dev.tonimatas.ethylene.mixins.mixinplugin.annotations.SelfConstructorStub;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.trading.ItemCost;
 import net.minecraft.world.item.trading.MerchantOffer;
 import org.bukkit.craftbukkit.v1_20_R5.inventory.CraftMerchantRecipe;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,6 +13,9 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.Optional;
+
+@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 @Mixin(MerchantOffer.class)
 public abstract class MerchantRecipeMixin implements MerchantOfferLink {
     @Shadow public abstract ItemStack getCostA();
@@ -23,11 +29,16 @@ public abstract class MerchantRecipeMixin implements MerchantOfferLink {
         return (bukkitHandle == null) ? bukkitHandle = new CraftMerchantRecipe((MerchantOffer) (Object) this) : bukkitHandle;
     }
     
-    // TODO: Constructor
-    //public MerchantRecipe(ItemCost baseCostA, Optional<ItemCost> costB, ItemStack result, int uses, int maxUses, int experience, float priceMultiplier, int demand, CraftMerchantRecipe bukkit) {
-    //    this(baseCostA, costB, result, uses, maxUses, experience, priceMultiplier, demand);
-    //    this.bukkitHandle = bukkit;
-    //}
+    @SuppressWarnings("MissingUnique")
+    @SelfConstructorStub
+    public abstract void init(ItemCost p_331744_, Optional<ItemCost> p_330460_, ItemStack p_324239_, int p_324562_, int p_324493_, int p_323558_, float p_323528_, int p_324484_);
+    
+    @Unique
+    @NewConstructor
+    public void init(ItemCost baseCostA, Optional<ItemCost> costB, ItemStack result, int uses, int maxUses, int experience, float priceMultiplier, int demand, CraftMerchantRecipe bukkit) {
+        init(baseCostA, costB, result, uses, maxUses, experience, priceMultiplier, demand);
+        this.bukkitHandle = bukkit;
+    }
     
     @Redirect(method = "take", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;shrink(I)V"))
     private void craftbukkit$take(ItemStack instance, int p_41775_) {
