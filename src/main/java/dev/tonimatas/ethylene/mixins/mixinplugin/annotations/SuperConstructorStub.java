30352a5f9a1531c4a84f4ab390c31f8a83f3445b
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-06-12 16:39:51 +0200

    Fix constructor

---
+++ src/main/java/dev/tonimatas/ethylene/mixins/mixinplugin/annotations/SuperConstructorStub.java
@@ -0,0 +1,8 @@
+package dev.tonimatas.ethylene.mixins.mixinplugin.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.CLASS)
+public @interface SuperConstructorStub {
+}
